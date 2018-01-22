3c436ad16808dc9cdd1c6fd165e5c234e431b9d8
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-22 18:49:40 -0200

    'up'

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
 //alert('teste');
-
+/*
 var conn = navigator.connection.type;
 if( conn == 'none' || conn == 'NONE' ){ //conn
 
@@ -13,13 +13,14 @@ alert('O aplicativo nao detectou conexao com internet.');
 } else {
 
 document.getElementById('conectando').style.display = 'none';
-cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
-
+//cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
 }
 
 
 document.getElementById('fechar').style.display = 'block';
+*/
 
+atualizar();
 
 }
 
@@ -27,6 +28,33 @@ document.getElementById('fechar').style.display = 'block';
 
 function fechar(){
 navigator.app.exitApp();
+}
+
+
+
+
+
+
+
+function atualizar(){
+
+
+var conn = navigator.connection.type;
+if( conn == 'none' || conn == 'NONE' ){ //conn
+
+alert('O aplicativo nao detectou conexao com internet.');
+
+} else {
+//cordova.InAppBrowser.open(encodeURI('http://fazerja.com.br/site/index.php?app=s'), '_self', 'location=no,hidden=no');
+
+var urlBrow = 'http://fazerja.com.br/site/index.php?app=s';
+//alert( 'urlBrow: ' + urlBrow );
+ifrBrowser.location.href =  urlBrow;
+
+}
+
+
+
 }
 	   
 
